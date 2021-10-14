ea6031af5d26479f845ffff445a01fa745e61d89
CoderZ90 <62499625+CoderZ90@users.noreply.github.com>
2021-10-14 16:49:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Javascript
+// No1. Styling Console.log() trick
+console.log('%cStyled Text In Console !!!', 'color: #fff; font-family: Poppins; font-size: 30px; font-weight: bold; background: black; padding: 20px');
+// About
+console.log('%cStyling console.log is a very easy part of javascript. The main purpose of styling the console.log is to prevent users to enter malicious code into the console to prevent them to get hacked or get any data from their account', 'color: red; font-size: 20px; margin-top: 20px;')
+// Who use it
+console.log('%cIt is common to see in many social media websites such as Facebook, Discord, etc where they have big red text appearing in the console saying "WARNING!" or similar to prevent you from entering malicious code.', 'color: #222; font-family: Poppins; font-size: 20px; line-height: 2rem; font-weight: 500')
